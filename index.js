0899efb438e98ecf83bcbe04f48665b3624bcf78
Staff GVA <GVA@Sach-Australia.local>
2019-12-27 05:37:17 +1100

    Gonna try git tags now

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const Koa = require('koa');
 const app = new Koa();
 
+// add a date method to the context
+app.context.date = Date();
+app.context.userData = {
+    first: 'Manny',
+    last: 'Henri',
+};
+
+// response
 app.use(ctx => {
-    ctx.body = 'Hi There';
+    // use state
+    ctx.state.user = 'Mrs Stateless';
+    // request obj usage
+    let from = ctx.request;
+   // print date 
+    ctx.response.body = ctx.userData;
+    console.log(typeof(from));
 });
 
 app.listen(3000);
